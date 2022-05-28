9fa61d27807d34c50412f148a3b7fa8e520f470b
Ahmed Abdelazeem <58098260+abdelazeem201@users.noreply.github.com>
2022-05-28 18:04:42 +0200

    Create mux.v

---
+++ ASS 3/Prob-2/mux.v
@@ -0,0 +1,13 @@
+module mux  ( input select, 
+              input [7:0] a, 
+              input [7:0] b, 
+              output reg [7:0] out);
+	
+  always@(*)
+	begin
+		if (select==0)
+			out=a;
+		else
+			out=b;
+	end
+endmodule
