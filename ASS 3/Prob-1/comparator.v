eff4eb430012b9f5cffe17766f8249b967cce163
Ahmed Abdelazeem <58098260+abdelazeem201@users.noreply.github.com>
2022-05-28 18:00:55 +0200

    Create comparator.v

---
+++ ASS 3/Prob-1/comparator.v
@@ -0,0 +1,13 @@
+module comparator ( input [6:0] a, 
+                    input [6:0] b, 
+                    output reg out);
+	
+	always@(*)
+	begin
+		if (a > b)
+			out=0;
+		else
+			out=1;
+	end
+endmodule
+	
